02c65796225ed0f6c147f01131b122605d948f73
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-09-15 07:33:26 +0200

    a bunch of stuff

--- script.js
+++ script.js
@@ -33,6 +33,12 @@ const aboutContainer = document.getElementById("About-container");
 const newsContainer = document.getElementById("News-container");
 const contactContainer = document.getElementById("Contact-container");
 
+//other buttons
+const Easteregg = document.querySelector("a.double-click-here");
+
+//text(a,p,h1,h2...)
+const homeMainText = document.querySelector("p.Home-main-paragraph");
+
 //base url
 const mainUrl = "https://index-ish.github.io/main/";
 
@@ -94,5 +100,24 @@ function Checker(){
 
 
 setInterval(Checker,100);
+
+
+//checks for double click
+Easteregg.addEventListener("dblclick", ()=>{
+    var row1 = [" "," "," "," "," "," "," "," "," "," "];
+    var row2 = [" "," "," "," "," "," "," "," "," "," "];
+    var row3 = [" "," "," "," "," "," "," "," "," "," "];
+    var row4 = [" "," "," "," "," "," "," "," "," "," "];
+    var row5 = [" "," "," "," "," "," "," "," "," "," "];
+    var row6 = [" "," "," "," "," "," "," "," "," "," "];
+    var row7 = [" "," "," "," "," "," "," "," "," "," "];
+    var row8 = [" "," "," "," "," "," "," "," "," "," "];
+    var row9 = [" "," "," "," "," "," "," "," "," "," "];
+    var row10 = [" "," "," "," "," "," "," "," "," "," "];
+    
+    homeMainText = row1[0] + row1[1] + row1[2] + row1[3] + row1[4] + row1[5] + row1[6] + row1[7] + row1[8] + row1[9] + "<br>" + row2[0] + row2[1] + row2[2] + row2[3] + row2[4] + row2[5] + row2[6] + row2[7] + row2[8] + row2[9] + "<br>" + row3[0] + row3[1] + row3[2] + row3[3] + row3[4] + row3[5] + row3[6] + row3[7] + row3[8] + row3[9] + "<br>" + row1[0] + row1[1] + row1[2] + row1[3] + row1[4] + row1[5] + row1[6] + row1[7] + row1[8] + row1[9] + "<br>" + row1[0] + row1[1] + row1[2] + row1[3] + row1[4] + row1[5] + row1[6] + row1[7] + row1[8] + row1[9] + "<br>" + row4[0] + row4[1] + row4[2] + row4[3] + row4[4] + row4[5] + row4[6] + row4[7] + row4[8] + row4[9] + "<br>" +
+
+
+})
 
 
